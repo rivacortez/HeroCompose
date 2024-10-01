a859dc25268feda6991ff0cb5a106ed9234479db
rivacortezf <u202215313@upc.edu.pe>
2024-10-01 15:59:19 -0500

    feat: added new view to switch favorites herores

--- app/src/main/java/pe/edu/upc/superherocompose/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/superherocompose/MainActivity.kt
@@ -4,18 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.room.Room
 import pe.edu.upc.superherocompose.common.Constants
 import pe.edu.upc.superherocompose.data.local.AppDatabase
 import pe.edu.upc.superherocompose.data.remote.HeroService
 import pe.edu.upc.superherocompose.data.repository.HeroRepository
+import pe.edu.upc.superherocompose.domain.Hero
 import pe.edu.upc.superherocompose.presentation.HeroListScreen
 import pe.edu.upc.superherocompose.presentation.HeroListViewModel
 import pe.edu.upc.superherocompose.ui.theme.SuperHeroComposeTheme
@@ -23,8 +35,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         val service = Retrofit
             .Builder()
@@ -36,16 +48,63 @@ class MainActivity : ComponentActivity() {
         val dao = Room
             .databaseBuilder(applicationContext, AppDatabase::class.java, "heroes-db")
             .build()
-            .getHeroDao()
+            .getHeroDao()// Cambié el nombre del método para acceder al DAO de héroes
 
-        val viewModel = HeroListViewModel(HeroRepository(service, dao))
+        val repository = HeroRepository(service, dao)
 
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             SuperHeroComposeTheme {
-                HeroListScreen(viewModel)
+                val items = listOf(
+                    ItemTab(
+                        "Search",
+                        Icons.Filled.Search
+                    ),
+                    ItemTab(
+                        "Favorites",
+                        Icons.Filled.Favorite
+                    )
+                )
+
+                val index = remember {
+                    mutableStateOf(0)
+                }
+
+                Scaffold { paddingValues ->
+                    Column(modifier = Modifier.padding(paddingValues)) {
+                        LazyRow {
+                            itemsIndexed(items) { position, item ->
+                                FilterChip(
+                                    selected = index.value == position,
+                                    leadingIcon = {
+                                        Icon(item.icon, contentDescription = item.name)
+                                    },
+                                    label = {
+                                        Text(item.name)
+                                    },
+                                    onClick = {
+                                        index.value = position
+                                    }
+                                )
+                            }
+                        }
+
+                        if (index.value == 0) {
+                            val heroViewModel = HeroListViewModel(repository)
+                            HeroListScreen(heroViewModel)  // Pantalla de búsqueda de héroes
+                        } else {
+                            val heroListViewModel = HeroListViewModel(repository)
+                            HeroListScreen(heroListViewModel)  // Pantalla de favoritos de héroes
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+data class ItemTab(
+    val name: String,
+    val icon: ImageVector
+)
